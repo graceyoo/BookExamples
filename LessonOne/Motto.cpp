af87aa03e5e6b3f15ccb76cc00da6ce959f50015
Grace Yoo <gy.graceyoo@gmail.com>
2020-07-08 23:44:07 -0400

    Completion of Hour 1 Lesson

---
+++ LessonOne/Motto.cpp
@@ -0,0 +1,13 @@
+/////////////////////////////////////////////////////////////
+/// Author: Grace
+/// Date: July 8, 2020
+/// Notes: Listing 1.1 (Pg. 8)
+/////////////////////////////////////////////////////////////
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "Solidum petit in profundis!\n";
+  return 0;
+}
