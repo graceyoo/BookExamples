98d89db17358441c04fbbdd5ded81fecc135d0af
Grace Yoo <gy.graceyoo@gmail.com>
2020-07-13 22:00:31 -0400

    Listing 3.3 - NewRectangle

--- LessonThree/NewRectangle.cpp
+++ LessonThree/NewRectangle.cpp
@@ -2,21 +2,23 @@
 /////////////////////////////////////////////////////////////
 /// Author: Grace
 /// Date: July 13, 2020
-/// Notes: Listing 3.2 (Pg. 34)
+/// Notes: Listing 3.3 (Pg. 35)
 /////////////////////////////////////////////////////////////
 
 #include <iostream>
 
 int main ()
 {
+  // Create a type definition
+  typedef unsigned short USHORT;
+  
   // Set up width and length
-  unsigned short width = 26, length;
-  length = 40;
+  USHORT width = 26;
+  USHORT length = 40;
   
   // Create an unsigned short initialized with the
   // result of multiplying width by length
-  
-  unsigned short area = width * length;
+  USHORT area = width * length;
   
   std::cout << "Width: " << width << "\n";
   std::cout << "Length: " << length << "\n";
